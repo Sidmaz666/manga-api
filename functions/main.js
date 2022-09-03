3b66aa4ca26b9a906e90af5eb9116d41ede7fa2c
sidmaz666 <sidmazumder8@gmail.com>
2022-09-03 06:50:18 +0530

    Adding Vercel JSON

--- functions/main.js
+++ functions/main.js
@@ -3,10 +3,17 @@ const axios = require('axios')
 const extract = require('./extract.js')
 
 const base_url = "https://mangabuddy.com/"
+const axios_header = {
+	headers : {
+	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+        'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        'referer': 'https://mangabuddy.com/',
+	}
+} 
 
 async function popular(res,page){
-
-  const req = await axios(`${base_url}popular?page=${page}`)
+  try{
+  const req = await axios(`${base_url}popular?page=${page}`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_list(data,page)
@@ -14,12 +21,16 @@ async function popular(res,page){
   res.status(200).json({
     data: list
   })
+
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function recent(res,page){
-
-  const req = await axios(`${base_url}latest?page=${page}`)
+try{
+  const req = await axios(`${base_url}latest?page=${page}`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_list(data,page)
@@ -27,12 +38,15 @@ async function recent(res,page){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function search(res,page,key){
-
-  const req = await axios(`${base_url}search?q=${key}&page=${page}`)
+try{
+  const req = await axios(`${base_url}search?q=${key}&page=${page}`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_list(data,page)
@@ -40,12 +54,15 @@ async function search(res,page,key){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function all_category(res){
-
-  const req = await axios(`${base_url}genres`)
+try{
+  const req = await axios(`${base_url}genres`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_category(data)
@@ -53,13 +70,16 @@ async function all_category(res){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 
 async function category(res,category){
-
-  const req = await axios(`${base_url}genres/${category}`)
+try{
+  const req = await axios(`${base_url}genres/${category}`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_category_list(data)
@@ -67,13 +87,16 @@ async function category(res,category){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 
 async function sortAlphaNumeric(res,charc,page){
-
-  const req = await axios(`${base_url}az-list/${charc}?page=${page}`)
+try{
+  const req = await axios(`${base_url}az-list/${charc}?page=${page}`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_list(data,page)
@@ -81,12 +104,15 @@ async function sortAlphaNumeric(res,charc,page){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function get_chapters(res,id){
-
-  const req = await axios(`${base_url}api/manga/${id}/chapters?source=detail`)
+try{
+  const req = await axios(`${base_url}api/manga/${id}/chapters?source=detail`,axios_header)
   const data = await req.data
   
   const list = await extract.extract_chapter(data)
@@ -94,11 +120,14 @@ async function get_chapters(res,id){
   res.status(200).json({
     data: list
   })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function convetURL2URI(res,url){
-
+try{
   const main_url = 'https://s1.mbcdnv1.xyz/file/img-mbuddy/manga/' + url.replaceAll('__','/')
 
   const req = await axios(main_url,{
@@ -122,11 +151,14 @@ async function convetURL2URI(res,url){
       const data = `data:${req.headers["content-type"]};base64,${buffer}`
 
       res.status(200).json({image_uri:data})
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function get_manga(res,chapter_id){
-	
+try{	
   const manga_id = chapter_id.replace(/_chapte.*/g,'').replace('_','')
     		   .replace(/_vol.*/g,'')
 
@@ -134,10 +166,10 @@ async function get_manga(res,chapter_id){
 
   const req = await axios.all([
     axios.get(
-    `${base_url}${manga_id}`
+    `${base_url}${manga_id}`, axios_header
   ),
     axios.get(
-    `${base_url}${chapter}`
+    `${base_url}${chapter}` , axios_header
   )
   ])
 
@@ -153,10 +185,14 @@ async function get_manga(res,chapter_id){
   )
 
   res.status(200).json({data :manga_})
+  } catch(err){
+    console.log(err)
+  }
 
 }
 
 async function get_thumb(res,id){
+  try{
     const req = await axios("https://thumb.youmadcdn.xyz/file/img-mbuddy/thumb/" + id,{
 	headers : {
         'authority': 'thumb.youmadcdn.xyz',
@@ -180,6 +216,9 @@ async function get_thumb(res,id){
       res.status(200).json({
 	image_uri : data
       })
+  } catch(err){
+    console.log(err)
+  }
 
 }
 

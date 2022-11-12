96f6df9396c41f710e18719fb9a6f90d652bc5d9
sidmaz666 <sidmazumder8@gmail.com>
2022-11-12 08:10:39 +0530

    adding bypass script

--- functions/bypass_cloudflare.js
+++ functions/bypass_cloudflare.js
@@ -1,39 +1,32 @@
 //Dependencies
-import puppeteer_Stealth from "puppeteer-extra-plugin-stealth";
-import puppeteer from "puppeteer-extra";
+import puppeteer_Stealth from "puppeteer-extra-plugin-stealth"
+import puppeteer from "puppeteer-extra"
 
 ///Configurations
 //puppeteer
-puppeteer.default.use(puppeteer_Stealth());
+puppeteer.default.use(puppeteer_Stealth())
 
 //Main
-export async function get(url, headers = {}, useragent = "") {
-  return new Promise(async (resolve) => {
-    const browser = await puppeteer.default.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--incognito",
-        "--single-process",
-        "--no-zygote",
-      ],
-    });
-    const page = await browser.newPage();
+export async function get(url, headers = {}, useragent = ""){
+    return new Promise(async(resolve) =>{
+        const browser = await puppeteer.default.launch({ headless: true, args: [
+	  "--no-sandbox", "--disable-setuid-sandbox", "--incognito" , "--single-process", "--no-zygote"] })
+        const page = await browser.newPage()
 
-    if (headers) {
-      await page.setExtraHTTPHeaders(headers);
-    }
+        if(headers){
+            await page.setExtraHTTPHeaders(headers)
+        }
 
-    if (useragent) {
-      await page.setUserAgent(useragent);
-    }
+        if(useragent){
+            await page.setUserAgent(useragent)
+        }
 
-    await page.goto(url, { waitUntil: "domcontentloaded", timeout: 0 });
+      await page.goto(url, {waitUntil: 'domcontentloaded', timeout : 0})
 
-    const page_content = await page.content();
+        const page_content = await page.content()
 
-    await browser.close();
-    resolve(page_content);
-  });
+        await browser.close()
+        resolve(page_content)
+    })
 }
+
